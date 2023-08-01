2851336fc39f8e44e07e6011190ac36b9c1d030e
Brittany Cowper <brittferren@gmail.com>
2023-07-31 20:20:31 -0500

    complete exercise 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,12 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+burnaby = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+richmond = Store.create(name: "Richmon", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+gastown = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+
+@store1 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2)
+
+@store1.mens_apparel = false
+
